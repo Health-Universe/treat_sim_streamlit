8fe5fd2fcb7b871e58bccc09a99647d4d3e1df42
CommunityDiscovery <166847950+CommunityDiscovery@users.noreply.github.com>
2024-04-17 15:01:06 -0500

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
-import urllib
+import urllib.request
 
 INFO_1 = '''**A simple simulation model of a urgent care and treatment centre.**'''
 OVERVIEW_PATH = 'txt/overview.md'
 
-@st.experimental_singleton(show_spinner=True)
+@st.cache_resource(show_spinner=True)
 def get_file_content_as_string(path):
     '''
     Download the content of a file from the GitHub Repo and return as a utf-8 string
